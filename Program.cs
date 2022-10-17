902b8db72bc98b531509066aab1d9f03ec98cb90
José Weliton Silva dos Santos <welitonkira@gmail.com>
2022-10-17 12:47:03 -0300

    Comecando a colocar pecas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using tabuleiro;
-
+using xadrez;
 
 namespace xadrez_console
 {
@@ -8,9 +8,13 @@ namespace xadrez_console
         private static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            
             Tela.ImprimirTabuleiro(tab);
             
             Console.WriteLine();
+
         }
     }
 }
---
+++ xadrez/Rei.cs
@@ -0,0 +1,18 @@
+﻿
+
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+            tab.peca(1, 5);
+        }
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
